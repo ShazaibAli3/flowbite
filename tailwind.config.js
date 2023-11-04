b8cadea802d77a488615073f150fbbc4616eedb4
Shahzaib Ali <shahzaibaali3@gmail.com>
2023-11-05 00:55:21 +0500

    sign in page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: "tw-",
-  content: ["index.html"],
+  content: ['index.html','pages/**/*.html'],
   theme: {
     extend: {},
   },
