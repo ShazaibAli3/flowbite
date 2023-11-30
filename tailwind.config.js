02539521c8abf7c8443ae71a402e050eba68d9af
Shahzaib Ali <shahzaibaali3@gmail.com>
2023-11-30 22:37:49 +0500

    wefsdfefre

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: "tw-",
-  content: ['index.html','pages/**/*.html'],
+  content: ['index.html', 'pages/**/*.html'],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        deeppurple: '#4C4667',
+        lightgray: '#FFFFFF75',
+        primarytext: '#FFFFFF',
+        primemain: '#4169E1',
+        crystalwhite: '#F4F4F4',
+        textsec: '#141414B2',
+        lgray: '#DFDFDF',
+        pbg: '#C8E6C95E',
+        bubor: '#4169E13D',
+        joinc: '#1414148F'
+      },
+      fontFamily: {
+
+        roboto: 'Roboto',
+      }
+    },
   },
   plugins: [],
 }
